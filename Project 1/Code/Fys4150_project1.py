cd57bbf55a7cce2555d8dcb73e5724c079987a92
Akselgra <31341369+Akselgra@users.noreply.github.com>
2019-09-09 12:29:56 +0200

    did first parts of project

--- Project 1/Code/Fys4150_project1.py
+++ Project 1/Code/Fys4150_project1.py
@@ -1,45 +1,88 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from time import time
 
 
-class Second_derivative():
+class Project_solver():
     
-    def __init__(self, v):
-        self.n = len(v) #length of array  we want to derivate
-        
-        
+    def __init__(self, f, a = -1, b = 2, c = -1, M = False):
+        self.n = len(f) 
+        self.h = 1/(self.n + 1)
+        self.f = f
+        self.a = a
+        self.b = b
+        self.c = c
+        if M == True:
+            self.M = M
+        else:
+            self.builder()
     def builder(self):
         """
-        Builds a matrix A
+        Builds a matrix M
         Used in the linear transformation
-        Av = f
-        to find the second derivatives of v
+        Mv = f
         """
-        init_array = np.zeros(self.n)
-        init_array[0] = 2; init_array[1] = -1; init_array[-1] = -1
-        
-        init_array_0 = np.zeros(self.n)
-        init_array_0[0] = 2; init_array_0[1] = -1;
-            
-        init_array_1 = np.zeros(self.n)
-        init_array_1[-2] = -1; init_array_1[-1] = 2
-        
-        A = []
-        A.append(init_array_0)
+        M = np.zeros((self.n, 4))
+        M[0, :] = np.array([self.b, self.c, 0, self.f[0]])
         for i in range(1, self.n-1):
-            A.append(np.roll(init_array, i))
-        A.append(init_array_1)
-        A = np.array(A)
-        self.A = A
+            M[i, :] = np.array([self.a, self.b, self.c, self.f[i]])
+        M[-1, :] = np.array([0, self.a, self.b, self.f[-1]])
+        self.M = M
+        self.M[:, :-1] = self.M[:, :-1]/self.h**2
+
+
+    def linear_equation_solver(self):
+        """
+        Solves Mv = f for v
+        """
+        self.M[0, :] = self.M[0, :]/self.M[0, 0]
+        self.M[1, :] = self.M[1, :] - self.M[0, :]*self.M[1, 0]
+        for i in range(1, self.n-2):
+            self.M[i, 1:] = self.M[i, 1:]/self.M[i, 1]
+            
+            multi_comp = self.M[i+1, 0]
+            for j in range(2):
+                self.M[i+1, j] = self.M[i+1, j] - self.M[i, j+1]*multi_comp
+            self.M[i+1, -1] = self.M[i+1, -1] - self.M[i, -1]*multi_comp
+
+        self.M[-2, :] = self.M[-2, :]/self.M[-2, 1]
+        self.M[-1, :] = self.M[-1, :] - self.M[-2, :]*self.M[-1, 1]
+        self.M[-1, :] = self.M[-1, :]/self.M[-1, -2]
+                
         
-
-
+        self.M[-2, :] = self.M[-2, :] - self.M[-1, :]*self.M[-2, -2]
+        
+        self.M[0, -1] = 0
+        for i in range(1, self.n-1):
+            multi_comp = self.M[-(i+1), -2]
+            self.M[-(i+1), -2] = \
+            self.M[-(i+1), -2] - self.M[-i, -3]*multi_comp
+            self.M[-(i+1), -1] = self.M[-(i+1), -1] - self.M[-i, -1]*multi_comp
+            
+        self.M[0, :] = self.M[0, :] - self.M[1, :]*self.M[0, 1]
+        self.v = self.M[:, -1]
+        self.M[-1, -1] = 0
+        self.M[0, -1] = 0
+        self.v = self.M[:, -1]
+        
+        
 if __name__ == "__main__":
-    x = np.linspace(0, 8*np.pi, 5000)
-    v = np.cos(x)
-    dx = x[1]-x[0]
-    objekt = Second_derivative(v)
-    objekt.builder()
-    out = objekt.A
-    func = np.dot(out, v)*dx**-2
-    plt.plot(x[1:-1], func[1:-1])
+    def trueplot(x):
+        return(1 - (1 - np.exp(-10))*x - np.exp(-10*x))
+    
+    def linear_equation_tester():
+        n_list = [int(1e1), int(1e2), int(1e3)]
+        for n in n_list:
+            x = np.linspace(0, 1, n)
+            f = 100*np.exp(-10*x)
+            objekt = Project_solver(f)
+            objekt.linear_equation_solver()
+            plt.plot(x, objekt.v)
+            plt.plot(x, trueplot(x))
+            plt.legend(["numerical", "closed-form solution"])
+            plt.title("Numerical vs closed-form for n = %g" % n)
+            plt.xlabel("x")
+            plt.ylabel("u(x)")
+            plt.show()
+        
+    linear_equation_tester()
